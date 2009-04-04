eac30e7f837b308fa0775f18b664781d0f402ee6
eggegg User <andrewliu33@gmail.com>
2009-04-05 00:12:53 +0800

    Some modified files.

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,7 +1,7 @@
 class Deal < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
-  has_one :dda
+  has_many :comments, :dependent => :destroy
+  has_one :dda, :dependent => :destroy
   acts_as_taggable_on :tags
   
   validates_uniqueness_of :title
